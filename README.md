8ccf02d0f344a517f962953c500342d1e40f23de
kanchenhao <azstaylorswift13@gmail.com>
2022-05-07 22:26:59 +0800

    version 0.02: 无需下载额外软件

--- README.md
+++ README.md
@@ -8,26 +8,26 @@ pip install -r requirements.txt
 
 ## 使用方法
 
-从[chromedriver](http://chromedriver.storage.googleapis.com/index.html)下载并解压得到`chromedriver.exe`，将其放在主程序同一文件夹下。
+~~从[chromedriver](http://chromedriver.storage.googleapis.com/index.html)下载并解压得到`chromedriver.exe`，将其放在主程序同一文件夹下。~~
 
 1. 方法一: PYTHON
 
 ```bash
-python connconnect_campus_network.py --user [用户名] --pwd [密码]
+python connconnect_campus_network.py --uid [用户名] --pwd [密码]
 ```
 
 2. 方法二: EXE
 
 由pyinstaller打包获得:
 
-```
+```bash
 pyinstaller -F connconnect_campus_network.py
 ```
 
-或者直接下载打包好的[connconnect_campus_network.exe](https://github.com/kanchenhao/auto-reconnet-campus-network/releases/tag/v0.0.1)至chromedriver.exe同一文件夹下.
+或者直接下载打包好的[connconnect_campus_network.exe](https://github.com/kanchenhao/auto-reconnet-campus-network/releases/tag/v0.0.2)~~至chromedriver.exe同一文件夹下~~.
 
 双击或者在cmd中运行如下命令:
 
 ```bash
-connconnect_campus_network.exe --user [用户名] --pwd [密码]
+connconnect_campus_network.exe --uid [用户名] --pwd [密码]
 ```
--- connect_campus_network.py
+++ connect_campus_network.py
@@ -1,13 +1,14 @@
 import argparse
-import os
 import subprocess
 import time
 import msvcrt
 import logging
 import schedule
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
+import requests
+import urllib.parse
+# import os
+# from selenium import webdriver
+# from selenium.webdriver.chrome.options import Options
 
 class Network:
   def __init__(self, userId, password):
@@ -39,30 +40,69 @@ class Network:
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
     # 0: 网络可连通, 1: 网络不可达
-    ret = True if backinfo == 0 else False
-    return ret
+    return backinfo == 0
 
-  def reconnect_network(self):
-    campus_network_url = "http://192.168.50.3:8080/"  # 校园网登录页面
-    chromedriver = "./chromedriver.exe"  # chromedriver.exe文件所在位置
-    os.environ["webdriver.chrome.driver"] = chromedriver
-    options = Options()
-    # 取消DevTools listening on ws://127.0.0.1...提示
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    options.add_argument("--headless")                             # 以无窗口模式运行chromedriver程序
-    browser = webdriver.Chrome(chromedriver, options=options)      # 模拟打开浏览器
-    browser.get(campus_network_url)                                # 打开校园网登录页面
-    browser.find_element_by_id("username").send_keys(self.userId)  # 输入账号
-    time.sleep(1)
-    browser.find_element_by_id("pwd_tip").click()                  # 点击登录显示隐藏密码输入框
-    time.sleep(1)
-    browser.find_element_by_id("pwd").send_keys(self.password)     # 输入密码
-    time.sleep(1)
-    browser.find_element_by_id("loginLink_div").click()            # 点击连接按钮
-    time.sleep(2)
-    reconnect_res = browser.title == "登录成功"
-    browser.quit()
-    return reconnect_res
+  def get_info(self):
+      redirect_host = 'http://123.123.123.123'
+      headers = {
+          "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+          "Accept-Encoding": "gzip, deflate",
+          "Accept-Language": "zh-CN,zh;q=0.9,tr;q=0.8,en-US;q=0.7,en;q=0.6",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+          "Upgrade-Insecure-Requests": "1",
+          "Host": "123.123.123.123",
+          "Proxy-Connection": "keep-alive"
+      }
+      info = {}
+      res = requests.get(redirect_host, headers=headers).content.decode()
+      info["url"] = res[res.find('http:'):res.find('eportal')]
+      info["querystr"] = res[(res.find("wlanuserip")):(res.find("'</script>"))]
+      if not info["querystr"]:
+          logging.info("网络似乎正常.")
+      # 需要进行encodeURI编码
+      info["querystr"] = urllib.parse.quote(info["querystr"])
+      return info
+
+  def relogin(self):
+      info = self.get_info()
+      url = info["url"] + "eportal/InterFace.do?method=login"
+      headers = {
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
+          "Content-Type": "application/x-www-form-urlencoded",
+          "charset": "UTF-8",
+      }
+      data = "userId=%s&password=%s&service=&queryString=%s&operatorPwd=&operatorUserId=&validcode=&passwordEncrypt=false" % (self.userId, self.password, info["querystr"])
+
+      session = requests.session()
+      req = session.post(url=url, headers=headers, data=data).json()
+      return req["result"] == "success"
+
+  # def reconnect_network(self):
+  #   campus_network_url = "http://192.168.50.3:8080/"  # 校园网登录页面
+  #   chromedriver = "./chromedriver.exe"  # chromedriver.exe文件所在位置
+  #   os.environ["webdriver.chrome.driver"] = chromedriver
+  #   options = Options()
+  #   # 取消DevTools listening on ws://127.0.0.1...提示
+  #   options.add_experimental_option('excludeSwitches', ['enable-logging'])
+  #   options.add_argument("--headless")                             # 以无窗口模式运行chromedriver程序
+  #   browser = webdriver.Chrome(chromedriver, options=options)      # 模拟打开浏览器
+  #   reconnect_res = False
+  #   try:
+  #     browser.get(campus_network_url)                                # 打开校园网登录页面
+  #     browser.find_element_by_id("username").send_keys(self.userId)  # 输入账号
+  #     time.sleep(1)
+  #     browser.find_element_by_id("pwd_tip").click()                  # 点击登录显示隐藏密码输入框
+  #     time.sleep(1)
+  #     browser.find_element_by_id("pwd").send_keys(self.password)     # 输入密码
+  #     time.sleep(1)
+  #     browser.find_element_by_id("loginLink_div").click()            # 点击连接按钮
+  #     time.sleep(2)
+  #     reconnect_res = browser.title == "登录成功"
+  #   except Exception as e:
+  #     print(e)
+  #   finally:
+  #     browser.quit()
+  #   return reconnect_res
 
   def run_loop(self):
     ping_baidu = 'www.baidu.com'  # 百  度
@@ -74,13 +114,14 @@ class Network:
       print("校园网未登录, 尝试重连中...")
       logging.info("校园网未登录, 尝试重连中...")
       if self.ping_network(ping_campus):
-        reconnect_res = self.reconnect_network()
+        reconnect_res = self.relogin()
+        # reconnect_res = self.reconnect_network()  # 使用chromium模拟登录
+        time.sleep(3)  # 等待3秒钟再判断是否登录成功
         if reconnect_res and self.ping_network(ping_baidu):
           print("校园网已使用账户%s重新登录." % self.userId)
           logging.info("校园网已使用账户%s重新登录." % self.userId)
         else:
           print("校园网未登录成功, 请检查账号密码是否输入正确!")
-          print("您输入的用户名是: %s 密码: %s" % (self.userId, self.password))
           logging.info("校园网未登录成功, 请检查账号密码是否输入正确!")
       else:
         print("无法访问校园网登录页面, 请检查是否连接网络!")
@@ -114,10 +155,10 @@ def input_pwd(hint_word):
 if __name__ == '__main__':
   # 输入校园网帐号和密码
   parser = argparse.ArgumentParser(description='校园网断开自动重连')
-  parser.add_argument('--user', type=str, default='', help='userId')
+  parser.add_argument('--uid', type=str, default='', help='userId')
   parser.add_argument('--pwd', type=str, default='', help='password')
   args = parser.parse_args()
-  userId = args.user
+  userId = args.uid
   password = args.pwd
   if userId == '' or password == '':
     print("请输入校园网账号密码!")
